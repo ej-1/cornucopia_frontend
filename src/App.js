bb9ac8444abe36cb9b0641b5f02dc8aa969a62ca
Erik <erikwjonsson@gmail.com>
2018-05-21 21:50:43 +0200

    Changed App and Layout back to being stateful.

--- src/App.js
+++ src/App.js
@@ -8,18 +8,22 @@ import Simulation from "./components/simulation/simulation";
 import RobotConfiguration from "./components/routes/robot-configuration";
 // https://daveceddia.com/create-react-app-express-backend/
 
-const App = () => {
-  <div className="App">
-    <Layout>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/simulate" exact component={Simulation} />
-          <Route path="/robotconfig" exact component={RobotConfiguration} />
-        </Switch>
-      </BrowserRouter>
-    </Layout>
-  </div>;
-};
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Layout>
+          <BrowserRouter>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/simulate" exact component={Simulation} />
+              <Route path="/robotconfig" exact component={RobotConfiguration} />
+            </Switch>
+          </BrowserRouter>
+        </Layout>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/layout.js
+++ src/layout.js
@@ -2,12 +2,16 @@ import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import React, { Component } from "react";
 
-const Layout = () => {
-  <div>
-    <Header />
-    {this.props.children}
-    <Footer />
-  </div>;
-};
+class Layout extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        {this.props.children}
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default Layout;

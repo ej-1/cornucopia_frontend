6858efd4401e3196b569427bc3945ccb72556aba
Erik <erikwjonsson@gmail.com>
2018-05-21 21:33:46 +0200

    Changed app and layout components into function, stateless components.

--- src/App.js
+++ src/App.js
@@ -8,22 +8,18 @@ import Simulation from "./components/simulation/simulation";
 import RobotConfiguration from "./components/routes/robot-configuration";
 // https://daveceddia.com/create-react-app-express-backend/
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Layout>
-          <BrowserRouter>
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/simulate" exact component={Simulation} />
-              <Route path="/robotconfig" exact component={RobotConfiguration} />
-            </Switch>
-          </BrowserRouter>
-        </Layout>
-      </div>
-    );
-  }
-}
+const App = () => {
+  <div className="App">
+    <Layout>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/simulate" exact component={Simulation} />
+          <Route path="/robotconfig" exact component={RobotConfiguration} />
+        </Switch>
+      </BrowserRouter>
+    </Layout>
+  </div>;
+};
 
 export default App;
--- src/layout.js
+++ src/layout.js
@@ -2,16 +2,12 @@ import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import React, { Component } from "react";
 
-class Layout extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        {this.props.children}
-        <Footer />
-      </div>
-    );
-  }
-}
+const Layout = () => {
+  <div>
+    <Header />
+    {this.props.children}
+    <Footer />
+  </div>;
+};
 
 export default Layout;

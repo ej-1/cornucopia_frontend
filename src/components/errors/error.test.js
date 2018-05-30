bd75dead8a503a9877d44e13e51a7cfd7228f154
Erik <erikwjonsson@gmail.com>
2018-05-31 01:07:30 +0200

    Added test for Error component when no props are sent. - Updated test title.

--- src/components/errors/error.test.js
+++ src/components/errors/error.test.js
@@ -2,9 +2,17 @@
 import React from "react";
 import Error from "../errors/error";
 import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 
-test("Error message gets rendered", () => {
+test("Error gets rendered", () => {
   const component = renderer.create(<Error message={"Danger! Danger!"} />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test("Error without message gets rendered", () => {
+  const wrapper = shallow(<Error />);
+  expect(wrapper.find("div").html()).toEqual(
+    '<div class="error">Something went wrong.</div>'
+  );
 });

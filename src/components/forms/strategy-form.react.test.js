2cfdbd08434317640ffae608288b12816244989f
Erik <erikwjonsson@gmail.com>
2018-05-30 22:44:11 +0200

    Cleaned up StrategyFrom a little. Added new test for dropdown, input and datepicker,

--- src/components/forms/strategy-form.react.test.js
+++ src/components/forms/strategy-form.react.test.js
@@ -6,10 +6,6 @@ import moment from "moment";
 
 const fetchSimulation = jest.fn();
 
-const form = shallow(<StrategyForm runSimulation={fetchSimulation} />);
-
-const component = renderer.create(<StrategyForm />);
-
 // Test that handleSubmit() calls parent's function.
 // Mock testing function call with arguments
 // https://stackoverflow.com/questions/43245040/using-jest-to-spy-on-method-call-in-componentdidmount
@@ -32,14 +28,9 @@ test("handleSubmit, sends correct data when state is set", () => {
 // Test that snapshot sets state attributes on clicking form submit button.
 test("User fills in form and submit, setting state fields", () => {
   const wrapper = mount(<StrategyForm runSimulation={fetchSimulation} />);
-  //snap.toMatchSpecificSnapshot('./specific/dir/my.shot');
-  let data = {
-    amount: "1000",
-    currencyPair: "BTC-USDT",
-    startDate: moment("2010-01-01"),
-    endDate: moment("2010-12-31"),
-    strategy: "MACD"
-  };
+
+  const amountInput = wrapper.find("#strategy-form-amount");
+  amountInput.hostNodes().simulate("change", { target: { value: 3344 } });
 
   const dropdownCurrency = wrapper.find("#dropdown-currency");
   dropdownCurrency
@@ -61,23 +52,75 @@ test("User fills in form and submit, setting state fields", () => {
     .simulate("change", { target: { value: moment("2012-01-01") } });
 
   const datepickerEnddate = wrapper.find("#datepicker-enddate");
-  datepickerEnddate;
-  wrapper
-    .find("#datepicker-enddate")
+  datepickerEnddate
     .hostNodes()
     .simulate("change", { target: { value: moment("2013-01-01") } });
 
-  wrapper.find("form").simulate("submit", data);
-
-  expect(wrapper.state()).toEqual({
+  let data_two = {
+    amount: 3344,
     currencyPair: "BTC-USDT",
     strategy: "MACD",
     startDate: moment("2012-01-01"),
     endDate: moment("2013-01-01")
-  });
+  };
 
-  expect(fetchSimulation).toHaveBeenCalledWith(data);
+  expect(wrapper.state()).toEqual(data_two);
+
+  wrapper.instance().handleSubmit();
+
+  expect(fetchSimulation).toHaveBeenCalledWith(data_two);
 });
+
+test("Input field value is changed", () => {
+  const wrapper = mount(<StrategyForm runSimulation={fetchSimulation} />);
+  const amountInput = wrapper.find("#strategy-form-amount");
+  amountInput.hostNodes().simulate("change", { target: { value: 3344 } });
+  expect(amountInput.hostNodes().html()).toEqual(
+    '<input type="number" id="strategy-form-amount" class="strategy-form-amount form-control" value="3344" placeholder="3344">'
+  );
+});
+
+//MOVE TEST TO DROPDOWN
+test("Dropdown value is changed on select", () => {
+  const wrapper = mount(<StrategyForm runSimulation={fetchSimulation} />);
+  const dropdownStrategy = wrapper.find("#dropdown-strategy");
+  dropdownStrategy
+    .find("ul .dropdown-menu li")
+    .at(0)
+    .find("a")
+    .simulate("click");
+
+  expect(dropdownStrategy.hostNodes().html()).toEqual(
+    '<button id="dropdown-strategy" role="button" aria-haspopup="true" aria-expanded="false" type="button" class="dropdown dropdown-toggle btn btn-default">MACD <span class="caret"></span></button>'
+  );
+});
+
+test("Datepicker startdate value is changed on change", () => {
+  const wrapper = mount(<StrategyForm runSimulation={fetchSimulation} />);
+  const datepickerStartdate = wrapper.find("#datepicker-startdate");
+  datepickerStartdate
+    .hostNodes()
+    .simulate("change", { target: { value: moment("2012-01-01") } });
+
+  expect(datepickerStartdate.hostNodes().html()).toEqual(
+    '<input type="text" id="datepicker-startdate" class="" value="2012/01/01 00:00">'
+  );
+});
+
+test("Datepicker enddate value is changed on change", () => {
+  const wrapper = mount(<StrategyForm runSimulation={fetchSimulation} />);
+  const datepickerEnddate = wrapper.find("#datepicker-enddate");
+  datepickerEnddate
+    .hostNodes()
+    .simulate("change", { target: { value: moment("2013-01-01") } });
+
+  expect(datepickerEnddate.hostNodes().html()).toEqual(
+    '<input type="text" id="datepicker-enddate" class="" value="2013/01/01 00:00">'
+  );
+});
+
+// USE THIS: https://reactjs.org/docs/test-renderer.html
+// https://github.com/jamiebuilds/react-test-renderer/blob/master/README.md
 
 // DON'T NEED TO TEST
 // that dropdown works as intended. Trust in react-bootstrap.

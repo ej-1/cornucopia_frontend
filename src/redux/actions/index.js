601bd98362884d15f7e70e2d2294c09985946bf1
Erik <erikwjonsson@gmail.com>
2018-06-20 11:45:46 +0200

    Removed documentation for thunk action creator.

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -30,28 +30,12 @@ export const receiveError = (data, json) => {
   };
 };
 
-// Meet our first thunk action creator!
-// Though its insides are different, you would use it just like any other action creator:
-// store.dispatch(fetchPosts('reactjs'))
-
+// Thunk action creator!
 export const fetchSimulation = data => {
   console.log("fetchSimulation ../actions/index.js----->", data);
-  // Thunk middleware knows how to handle functions.
-  // It passes the dispatch method as an argument to the function,
-  // thus making it able to dispatch actions itself.
 
   return function(dispatch) {
-    // First dispatch: the app state is updated to inform
-    // that the API call is starting.
-
     dispatch(requestSimulation(data));
-
-    // The function called by the thunk middleware can return a value,
-    // that is passed on as the return value of the dispatch method.
-
-    // In this case, we return a promise to wait for.
-    // This is not required by thunk middleware, but it is convenient for us.
-
     return simulate(data).then(response => {
       console.log("API RESPONSE", response);
       if (response.error) {
@@ -60,18 +44,5 @@ export const fetchSimulation = data => {
         dispatch(receiveSimulation(data, response));
       }
     });
-    // Do not use catch, because that will also catch
-    // any errors in the dispatch and resulting render,
-    // causing a loop of 'Unexpected batch number' errors.
-    // https://github.com/facebook/react/issues/6895
-    //error => console.log('An error occurred.', error)
-    //)
-    //.then(json =>
-    // We can dispatch many times!
-    // Here, we update the app state with the results of the API call.
-
-    //  dispatch(receivePosts(subreddit, json))
-    //)
-    //}
   };
 };

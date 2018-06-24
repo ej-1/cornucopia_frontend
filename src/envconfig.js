dc0044e7fee2b3c7faf08aec0de735f9f1034472
Erik <erikwjonsson@gmail.com>
2018-06-24 16:22:17 +0200

    Added a console log statemnt for debugging heroku.

--- src/envconfig.js
+++ src/envconfig.js
@@ -1,5 +1,6 @@
 export const getApiUrl = () => {
   const browser_url = window.location.href;
+  console.log("CHECK HERE IT IS THE BROWSER URL", browser_url);
   if (browser_url.includes("localhost:3000")) {
     return "http://localhost:3001";
   } else if (browser_url.includes("cornucopia-frontend.herokuapp.com")) {

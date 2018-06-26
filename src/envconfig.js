58f14f4c9940d7752e16ea5107b9b659d0d2ec86
Erik <erikwjonsson@gmail.com>
2018-06-26 03:21:27 +0200

    Changed how browser url or hostname is gotten for API url config.

--- src/envconfig.js
+++ src/envconfig.js
@@ -1,7 +1,7 @@
 export const getApiUrl = () => {
-  const browser_url = document.URL;
+  const browser_url = window && window.location && window.location.hostname;
   console.log("CHECK HERE IT IS THE BROWSER URL", browser_url);
-  if (browser_url.includes("localhost:3000")) {
+  if (browser_url.includes("localhost")) {
     return "http://localhost:3001";
   } else if (browser_url.includes("cornucopia-frontend.herokuapp.com")) {
     return "https://cornucopia-backend.herokuapp.com";

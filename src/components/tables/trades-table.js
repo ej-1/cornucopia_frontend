cfc5db882d2ec27b109fca3a6c3cf834ce0ecd1e
Erik <erikwjonsson@gmail.com>
2018-06-29 02:05:44 +0200

    Changed how style is imported.

--- src/components/tables/trades-table.js
+++ src/components/tables/trades-table.js
@@ -4,7 +4,7 @@ import { Table } from "react-bootstrap";
 
 import TradesRowBuy from "../tables/trades-row-buy";
 import TradesRowSell from "../tables/trades-row-sell";
-import style from "../tables/trades-table.css";
+import "../tables/trades-table.css";
 
 class TradesTable extends React.PureComponent {
   render() {

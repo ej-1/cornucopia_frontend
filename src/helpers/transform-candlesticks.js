b2f3ac0b2f109e816e9534784998eddb3c98f493
Erik <erikwjonsson@gmail.com>
2018-08-29 18:08:02 +0200

    Added formatting so 6 decimals are used for candlesticks data. Changed text-align for table.

--- src/helpers/transform-candlesticks.js
+++ src/helpers/transform-candlesticks.js
@@ -5,6 +5,26 @@ const transformCandleSticks = candleSticks => {
   for (let i = 0; i < cs.length; i++) {
     let candleStick = cs[i];
     candleStick.date = convertDate(candleStick.date);
+
+    // Change the number of decimals to 6 decimals.
+    candleStick.close = candleStick.close.toFixed(6);
+    candleStick.ema12 = candleStick.ema12
+      ? candleStick.ema12.toFixed(6)
+      : undefined;
+    candleStick.ema26 = candleStick.ema26
+      ? candleStick.ema26.toFixed(6)
+      : undefined;
+    candleStick.high = candleStick.high.toFixed(6);
+    candleStick.low = candleStick.low.toFixed(6);
+    candleStick.netProfit = candleStick.netProfit.toFixed(6);
+    candleStick.volumeFrom = candleStick.volumeFrom.toFixed(6);
+    candleStick.volumeTo = candleStick.volumeTo.toFixed(6);
+    if (candleStick.trade) {
+      candleStick.trade.tradePriceGain = candleStick.trade.tradePriceGain.toFixed(
+        6
+      );
+    }
+
     if (candleStick.trade) {
       const trade = candleStick.trade;
       trade.tradePriceGain = Number(parseFloat(trade.tradePriceGain));
